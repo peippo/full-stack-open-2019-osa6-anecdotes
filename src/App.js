b486bde017c5736116b61c17635081f5092cc705
Janne Peippo <peippo@lumous.com>
2019-11-02 13:15:25 +0200

    Add osa 6a 6.3 answer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React from "react";
 
-const App = (props) => {
-  const anecdotes = props.store.getState()
+const App = props => {
+	const anecdotes = props.store.getState();
+	const store = props.store;
 
-  const vote = (id) => {
-    console.log('vote', id)
-  }
+	const vote = id => {
+		store.dispatch({
+			type: "VOTE",
+			data: {
+				id
+			}
+		});
+	};
 
-  return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h2>Anecdotes</h2>
+			{anecdotes.map(anecdote => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => vote(anecdote.id)}>vote</button>
+					</div>
+				</div>
+			))}
+			<h2>create new</h2>
+			<form>
+				<div>
+					<input />
+				</div>
+				<button>create</button>
+			</form>
+		</div>
+	);
+};
 
-export default App
+export default App;
